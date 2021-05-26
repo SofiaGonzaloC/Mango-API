a28f8d8b8be6e687edf3473489c4f875b05b52ac
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-05-26 09:55:49 -0600

    db_config actualizado

--- db_config.py
+++ db_config.py
@@ -6,7 +6,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-client = pymongo.MongoClient(
-    f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@classexample0.pptb0.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
-    )
-db = client.dbExample0
+client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@db-example.pptb0.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
+db = client.test
+
+db = client.Badge
