5223cb087f727d35b972defac543cb231c61cb13
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-12 20:27:01 -0600

    '/all/users_names/' added

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ from flask import Flask, jsonify, request
 from flask_restful import Api
 from flask_pymongo import pymongo
 import db_config as database
+from flask_cors import CORS
 
 # Resources
 from res.badge import Badge
 from res.badges import Badges
+from res.posts import Posts
 
 app=Flask(__name__) #instance a new flask application
 api =Api(app)
+CORS(app)
 
 @app.route('/all/adults/')
 def get_adults():
-    response = list(database.db.Badges.find({"$gte": 21}))
+    response = list(database.db.Badges.find({'age': {"$gte": "21"}}))
 
     for document in response:
         document["_id"] = str(document['_id'])
@@ -21,7 +24,7 @@ def get_adults():
 
 @app.route('/all/projection/')
 def get_name_and_age():
-    response = list(database.db.Badges.find({'age': {"$gte": 20}}, {'name': 1, 'age':1}))
+    response = list(database.db.Badges.find({'age': {"$gte": "21"}}, {'name': "1", 'age':"1"}))
 
     for document in response:
         document["_id"] = str(document['_id'])
@@ -30,15 +33,25 @@ def get_name_and_age():
 
 @app.route('/all/kids')
 def get_kids():
-    response = list(database.db.Badges.find({"$gte": 1}))
+    response = list(database.db.Badges.find({'age': {"$lt": "20"}}))
 
     for document in response:
-        documnet["_id"] = str(document['_id'])
+        document["_id"] = str(document['_id'])
 
     return jsonify(response)
 
+@app.route('/all/users_names/')
+def get_all_names():
+    response = list(database.db.Badges.find({}, {'name':1}))
+
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify
+
 api.add_resource(Badge, '/new/','/<string:by>:<string:data>')
-api.add_resource(Badges, '/all/', '/delete/all/')
+api.add_resource(Badges, '/all/', '/delete/all/') # Badges = AllBadges, working for now
+api.add_resource(Posts, '/new/post/<string:_id>', '/post/<string:_id>', '/<string:_id>/<string:uuid>')
 
 if __name__ == '__main__':
     app.run(load_dotenv=True)
