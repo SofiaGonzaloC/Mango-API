ce0830dc6c2f6cb367a019d39026ba909bc80e54
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-12 20:27:39 -0600

    latest changes

--- res/badges.py
+++ res/badges.py
@@ -15,8 +15,8 @@ class Badges(Resource):
         return jsonify(response)
 
     def post(self):
-        _ids = list(database.db.Badges,insert_many([
-            {
+        _ids = list(database.db.Badges.insert_many([
+            
                {
                 'header_img_url': request.json[0]['header_img_url'],
                 'profile_picture_url': request.json[0]['profile_picture_url'],
@@ -25,8 +25,7 @@ class Badges(Resource):
                 'city': request.json[0]['city'],
                 'followers': request.json[0]['followers'],
                 'likes': request.json[0]['likes'],
-                'post': request.json[0]['post'],
-                'posts':request.json[0]['posts']
+                'post': request.json[0]['post']
                 },
                {
                 'header_img_url': request.json[1]['header_img_url'],
@@ -36,8 +35,7 @@ class Badges(Resource):
                 'city': request.json[1]['city'],
                 'followers': request.json[1]['followers'],
                 'likes': request.json[1]['likes'],
-                'post': request.json[1]['post'],
-                'posts':request.json[1]['posts']
+                'post': request.json[1]['post']
                 },
                {
                 'header_img_url': request.json[2]['header_img_url'],
@@ -47,8 +45,7 @@ class Badges(Resource):
                 'city': request.json[2]['city'],
                 'followers': request.json[2]['followers'],
                 'likes': request.json[2]['likes'],
-                'post': request.json[2]['post'],
-                'posts':request.json[2]['posts']
+                'post': request.json[2]['post']
                 },
                {
                 'header_img_url': request.json[3]['header_img_url'],
@@ -58,38 +55,35 @@ class Badges(Resource):
                 'city': request.json[3]['city'],
                 'followers': request.json[3]['followers'],
                 'likes': request.json[3]['likes'],
-                'post': request.json[3]['post'],
-                'posts':request.json[3]['posts']
-                },
-               {
-                'header_img_url': request.json[4]['header_img_url'],
-                'profile_picture_url': request.json[4]['profile_picture_url'],
-                'name': request.json[4]['name'],
-                'age': request.json[4]['age'],
-                'city': request.json[4]['city'],
-                'followers': request.json[4]['followers'],
-                'likes': request.json[4]['likes'],
-                'post': request.json[4]['post'],
-                'posts':request.json[4]['posts']
-                },
-               {
-                'header_img_url': request.json[5]['header_img_url'],
-                'profile_picture_url': request.json[5]['profile_picture_url'],
-                'name': request.json[5]['name'],
-                'age': request.json[5]['age'],
-                'city': request.json[5]['city'],
-                'followers': request.json[5]['followers'],
-                'likes': request.json[5]['likes'],
-                'post': request.json[5]['post'],
-                'posts':request.json[5]['posts']
-                },
-            }
+                'post': request.json[3]['post']
+                }
+            #   {
+            #     'header_img_url': request.json[4]['header_img_url'],
+            #     'profile_picture_url': request.json[4]['profile_picture_url'],
+            #     'name': request.json[4]['name'],
+            #     'age': request.json[4]['age'],
+            #     'city': request.json[4]['city'],
+            #     'followers': request.json[4]['followers'],
+            #     'likes': request.json[4]['likes'],
+            #     'post': request.json[4]['post']
+            #     },
+            #    {
+            #     'header_img_url': request.json[5]['header_img_url'],
+            #     'profile_picture_url': request.json[5]['profile_picture_url'],
+            #     'name': request.json[5]['name'],
+            #     'age': request.json[5]['age'],
+            #     'city': request.json[5]['city'],
+            #     'followers': request.json[5]['followers'],
+            #     'likes': request.json[5]['likes'],
+            #     'post': request.json[5]['post']
+            #     }
+            
         ]).inserted_ids)
 
         results = []
 
         for _id in _ids:
-            result.append(str(_id))
+            results.append(str(_id))
 
         return jsonify({'inserted_ids': results})
 
