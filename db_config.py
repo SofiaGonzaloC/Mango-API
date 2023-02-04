33317c7690fd6beec13a4282271d54fb99afe50d
Sofía González <sofiagonzaloc@outlook.com>
2023-02-04 16:50:39 -0600

    Changed database info

--- db_config.py
+++ db_config.py
@@ -6,7 +6,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@db-example.pptb0.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
+client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_NAME}.pptb0.mongodb.net/?retryWrites=true&w=majority")
 
 #Down here you need to add the collection name
-db = client.Badges
+db = client['Badges']
